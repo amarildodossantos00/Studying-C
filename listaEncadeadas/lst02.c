c07366f948c33cfd4518778c02e40b81d7cb4bcf
alien <alien@amdos-sa>
2024-08-07 09:06:10 -0400

    lst02

--- listaEncadeadas/lst02.c
+++ listaEncadeadas/lst02.c
@@ -1,3 +1,32 @@
 #include "list.h"
 
 // Procedimento para inserir no fim da lista
+
+void	inserir_no_fim(Pessoa **lista, int num)
+{
+	Pessoa *nova = malloc(sizeof(Pessoa));
+	Pessoa *aux;
+
+	if (nova)
+	{
+		nova->idade = num;
+		nova->proximo = NULL;
+		// é o primeiro?
+		if(*lista == NULL)
+			*lista = nova;
+		else
+		{
+			aux = *lista;
+			while (aux->proximo)
+				aux = aux->proximo;
+			aux->proximo = nova;
+		}
+	}
+	else
+		printf("Erro ao alocar memoria\n");
+}
+
+int	main(void)
+{
+
+}
---
+++ listaEncadeadas/lst03.c
@@ -0,0 +1,13 @@
+#include "list.h"
+
+// Procedimento para inserir no meio da lista
+
+void	inserir_no_meio(Pessoa **lista, int num)
+{
+
+}
+
+int	main(void)
+{
+
+}
