be97676493701fc48c3aab2d41ee9227a0b087f2
alien <alien@amdos-sa>
2024-08-07 09:26:14 -0400

    lst03

--- listaEncadeadas/lst03.c
+++ listaEncadeadas/lst03.c
@@ -2,9 +2,31 @@
 
 // Procedimento para inserir no meio da lista
 
-void	inserir_no_meio(Pessoa **lista, int num)
+void	inserir_no_meio(Pessoa **lista, int num, int ant)
 {
+	Pessoa *nova = malloc(sizeof(Pessoa));
+	Pessoa *aux;
 
+	if (nova)
+	{
+		nova->idade = num;
+		// e o primeiro
+		if (*lista == NULL)
+		{
+			nova->proximo = NULL;
+			*lista = nova;
+		}
+		else
+		{
+			aux = *lista;
+			while (aux->idade != ant && aux->proximo)
+				aux = aux->proximo;
+			nova->proximo = aux->proximo;
+			aux->proximo = nova;
+		}
+	}
+	else
+		printf("Erro ao alocar memoria!!!\n");
 }
 
 int	main(void)
